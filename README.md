219f8da691c34ee4a6536bb49da75e274bb8855e
sahilshirodkar <sahilshirodkar1630@gmail.com>
2025-10-26 19:37:41 +0530

    Added Validation for SignIn SignUp Form

--- README.md
+++ README.md
@@ -2,25 +2,27 @@
 
 - Create React App
 - Configured Tailwind CSS
+- Routing
 - Create Header
 - Login Page
 - SignUp Form
-
-
+- Form Validation
+- useRef Hook
 
 # Features
-Login/Signup Page 
-    -Sign In/ Sign Up Form
-    - Once logged in Redirect to Browse Page
+
+Login/Signup Page
+-Sign In/ Sign Up Form - Once logged in Redirect to Browse Page
 
 - Browse(after authentication)
-    - Header
-    - Main Movie
-        - Trailer in background
-        - Movie Title and description
-        - Movies Suggestions
-            - Movies List * N
+
+  - Header
+  - Main Movie
+    - Trailer in background
+    - Movie Title and description
+    - Movies Suggestions
+      - Movies List \* N
 
 - Netflix GPT
-    - Search Bar
-    - Movie Suggestions
+  - Search Bar
+  - Movie Suggestions
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,30 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import Header from "./Header";
+import { checkValidData } from "../utils/validate";
 
 const Login = () => {
+  const [isSignInForm, SetIsSignInForm] = useState(true);
+  const [errorMessage,setErrorMessage] = useState(null);
 
-  const [isSignInForm,SetIsSignInForm] = useState(true);
+  const name = useRef(null)
+  const email = useRef(null)
+  const password = useRef(null)
+
+  const handleButtonClick = () =>{
+    // Validate Form Data
+    const message = checkValidData(email.current.value,password.current.value)
+    setErrorMessage(message);
+    console.log(email.current.value);
+    console.log(password.current.value);
+    console.log(message);
+
+
+    //Sign In / Sign Up
+  }
 
   const toggleSignInForm = () => {
-    SetIsSignInForm(!isSignInForm)
-  }
+    SetIsSignInForm(!isSignInForm);
+  };
   return (
     <div className="relative h-screen w-full m-0 overflow-hidden">
       <Header />
@@ -17,29 +34,46 @@ const Login = () => {
           alt="logo"
         />
       </div>
-      <form className="absolute w-3/12 py-12 px-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80">
-        <h1 className="font-bold text-3xl py-2">{isSignInForm? "Sign In" : "Sign Up"}</h1>
+      <form onSubmit={e => e.preventDefault()}className="absolute w-3/12 py-12 px-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80">
+        <h1 className="font-bold text-3xl py-2">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </h1>
         {!isSignInForm && (
-        <input
-          type="text"
-          placeholder="Full Name"
-          className="p-4 my-4 w-full bg-gray-800"
-        />
+          <input
+          ref={name}
+            type="text"
+            placeholder="Full Name"
+            className="p-4 my-4 w-full bg-gray-800"
+          />
         )}
         <input
+        ref={email}
           type="text"
           placeholder="Email Address"
           className="p-4 my-4 w-full bg-gray-800"
         />
         <input
-          type="text"
+        ref={password}
+          type="Password"
           placeholder="Password"
           className="p-4 my-4 w-full bg-gray-800"
         />
-        <button className="p-4 my-4 bg-red-700 w-full rounded-lg">
-          {isSignInForm? "Sign In" : "Sign Up"}
+
+        <p className=" py-2 text-red-500 font-bold text-lg">{errorMessage}</p>
+
+        <button
+          className="p-4 my-4 bg-red-700 w-full rounded-lg"
+          onClick={handleButtonClick}
+        >
+          {isSignInForm ? "Sign In" : "Sign Up"}
         </button>
-        <p className="py-4 cursor-pointer" onClick={toggleSignInForm}> {isSignInForm? "New to Netflix? Sign Up Now" : "Already Registered? Sign In Now"}</p>
+
+        <p className="py-4 cursor-pointer" onClick={toggleSignInForm}>
+          {" "}
+          {isSignInForm
+            ? "New to Netflix? Sign Up Now"
+            : "Already Registered? Sign In Now"}
+        </p>
       </form>
     </div>
   );
