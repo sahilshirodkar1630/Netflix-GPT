e0599caea413f04a5ca0acfb8f406634cf2e8f87
sahilshirodkar <sahilshirodkar1630@gmail.com>
2025-12-01 01:15:30 +0530

    video title css fix

--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -7,7 +7,7 @@ const VideoTitle = ({title,overview}) => {
             <p className='hidden md:inline-block py-6 text-lg w-1/4'>{overview}</p>
              <div className="flex gap-3 my-4 md:my-0">
                 {/* Play Button */}
-                <button className="bg-white text-black font-medium py-1 md:py-4 px-2 md:px-12 text-xl rounded-lg flex items-center hover:bg-gray-300 transition">
+                <button className="bg-white text-black font-medium py-1 md:py-4 px-2 md:px-10 text-xl rounded-lg flex items-center hover:bg-gray-300 transition">
                     <svg 
                         xmlns="http://www.w3.org/2000/svg" 
                         fill="currentColor" 
@@ -20,7 +20,7 @@ const VideoTitle = ({title,overview}) => {
                     </button>
 
                     {/* More Info Button */}
-                    <button className=" hidden md:inline-block bg-gray-500/70 text-white font-medium p-4 px-12 text-xl rounded-lg flex items-center hover:bg-gray-500 transition">
+                    <button className=" hidden md:flex bg-gray-500/70 text-white font-medium p-4 px-12 text-xl rounded-lg flex flex-row items-center gap-2 hover:bg-gray-500 transition">
                     <svg 
                         xmlns="http://www.w3.org/2000/svg" 
                         fill="currentColor" 
